ece68fa89bc74713de7813d862747b10767003f5
Vinicius Costa <denis.vini@hotmail.com>
2021-05-04 17:35:34 +0100

    Exemplo

---
+++ source/src/pt/ubi/di/Aplicacao2.java
@@ -0,0 +1,28 @@
+package SI;
+
+import Model.Validations;
+
+public class Aplicacao2 {
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println("============== Opção =================");
+        System.out.println("======== (1) Cliente =================");
+        System.out.println("======== (2) Servidor ================");
+        System.out.println("======== (3) Sair ====================");
+        System.out.println("======================================");
+        String answer = Validations.readString();
+
+        switch (answer) {
+            case "1":
+                Cliente c = new Cliente();
+                break;
+            case "2":
+                Servidor servidor = new Servidor();
+                servidor.ConectarCliente(2222);
+                break;
+            case "3":
+                break;
+        }
+    }
+}
--- source/src/pt/ubi/di/Main.java
+++
@@ -1,8 +0,0 @@
-package pt.ubi.di;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
