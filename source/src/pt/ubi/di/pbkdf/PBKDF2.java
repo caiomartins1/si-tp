b25a24bdc107d0fae581f388b5fb76754e2f3d23
Caio Martins <dev.caiomartins@gmail.com>
2021-05-10 17:31:44 +0100

    Re-implemented hash method to accept the hash algorithm from user

--- source/src/pt/ubi/di/pbkdf/PBKDF2.java
+++ source/src/pt/ubi/di/pbkdf/PBKDF2.java
@@ -12,7 +12,7 @@ public class PBKDF2 {
     public static void main(String[] args) {
 
         // Example of usage (Java chars are 2 bytes sized)
-        System.out.println(hashPassword("password", "1234", 10000, 512));
+        System.out.println(hashPassword("sha1", "password", "1234", 10000, 512));
     }
 
     /**
@@ -38,25 +38,28 @@ public class PBKDF2 {
      * @param password   String password from which a derived key is generated
      * @param salt       String cryptographic salt (recommended: at least 64 bits)
      * @param iterations int number of iterations (recommended values: 4096, 2000, 10000, 5000 100000)
-     * @param keyLength  int length of the generated key (in bits)
+     * @param keyLength  int length of the generated key in bits (recommended: 512)
      * @return String representation of derived key generated
      * <p>
-     * source: https://medium.com/@kasunpdh/how-to-store-passwords-securely-with-pbkdf2-204487f14e84
+     *    Examples of pseudorandom function for PBKDF2 include
+     *    HMAC with SHA-1, SHA-224, SHA-256, SHA-384, SHA-512, SHA-512/224, and
+     *    SHA-512/256.  Applications may employ other schemes as well.
      */
-    public static String hashPassword(String password, String salt, int iterations, int keyLength) {
+    public static String hashPassword(String algorithm, String password, String salt, int iterations, int keyLength) {
 
         char[] passwordChars = password.toCharArray();
         byte[] saltBytes = salt.getBytes();
 
         try {
-            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHMAC" + algorithm);
             PBEKeySpec spec = new PBEKeySpec(passwordChars, saltBytes, iterations, keyLength);
             SecretKey key = skf.generateSecret(spec);
             byte[] res = key.getEncoded();
 
             return byteArrayToHex(res);
         } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-            throw new RuntimeException(e);
+            System.out.println("The provided algorithm is not valid");
+            return "";
         }
     }
 }
