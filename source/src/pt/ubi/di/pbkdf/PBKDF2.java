760f967884727c3aea4fdcd687d5160bdcf76391
Caio Martins <dev.caiomartins@gmail.com>
2021-05-18 18:13:08 +0100

    Added handleParams method

--- source/src/pt/ubi/di/pbkdf/PBKDF2.java
+++ source/src/pt/ubi/di/pbkdf/PBKDF2.java
@@ -7,14 +7,8 @@ import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
-//TODO: Maybe implement from scratch?
-//TODO: Choose different hash functions 
-public class PBKDF2 {
-    public static void main(String[] args) {
 
-        // Example of usage (Java chars are 2 bytes sized)
-        System.out.println(hashPassword("sha1", "password", "1234", 10000, 512));
-    }
+public class PBKDF2 {
 
     /**
      * @param byteArr byte array to be converted to a hex String
@@ -34,20 +28,18 @@ public class PBKDF2 {
         }
     }
 
-
     /**
      * @param password   String password from which a derived key is generated
-     * @param salt       String cryptographic salt (recommended: at least 64 bits)
      * @param iterations int number of iterations (recommended values: 4096, 2000, 10000, 5000 100000)
      * @param keyLength  int length of the generated key in bits (recommended: 512)
      * @return String representation of derived key generated
      * <p>
-     *    Examples of pseudorandom function for PBKDF2 include
-     *    HMAC with SHA-1, SHA-224, SHA-256, SHA-384, SHA-512, SHA-512/224, and
-     *    SHA-512/256.
+     * Examples of pseudorandom function for PBKDF2 include
+     * HMAC with SHA-1, SHA-224, SHA-256, SHA-384, SHA-512
      */
-    public static String hashPassword(String algorithm, String password, String salt, int iterations, int keyLength) {
+    public static String hashPassword(String algorithm, String password, int iterations, int keyLength) {
 
+        String salt = "123456789abcdef123456789abcdef"; // String cryptographic salt (recommended: at least 64 bits)
         char[] passwordChars = password.toCharArray();
         byte[] saltBytes = salt.getBytes();
 
@@ -57,10 +49,50 @@ public class PBKDF2 {
             SecretKey key = skf.generateSecret(spec);
             byte[] res = key.getEncoded();
 
+
             return byteArrayToHex(res);
         } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
             System.out.println("The provided algorithm is not valid");
             return "";
         }
     }
+
+
+    /**
+     * Handles the params given by the user
+     * @param args array of strings with flags and its respective values
+     * In case of only providing the password, this method sets default values for:
+     *             - Algorithm (SHA-1)
+     *             - Iterations (1000)
+     *             - Key Length (512 bits)
+     */
+    public static void handlePBKDFParams(String[] args) {
+        String algo = "sha1";
+        String pass = "";
+        int iter = 1000;
+        int keyLength = 512;
+
+        for (int i = 1; i < args.length; i++) {
+            if (args[i].equals("-a")) {
+                algo = args[i + 1];
+            }
+            if (args[i].equals("-p")) {
+                pass = args[i + 1];
+            }
+            if (args[i].equals("-i")) {
+                iter = Integer.parseInt(args[i + 1]);
+            }
+            if (args[i].equals("-l")) {
+                keyLength = Integer.parseInt(args[i + 1]);
+            }
+        }
+
+        if (pass.equals("")) {
+            System.out.println("Must provide a password -> check -help");
+            return;
+        }
+
+        System.out.println((hashPassword(algo, pass, iter, keyLength)));
+    }
+
 }
